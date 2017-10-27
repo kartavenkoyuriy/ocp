94b5921e18540c3deb1b206e9c8a04fbe548829e
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2017-10-27 19:36:23 +0300

    chapter 2. design patterns. encapsulating

---
+++ src/main/java/chapter2/design/Animal.java
@@ -0,0 +1,37 @@
+package chapter2.design;
+
+//imagine that we are designing a new Animal class, and we have the following
+//        design requirements:
+//        ■ Each animal has a non‐null, non‐empty species field
+//        ■ Each animal has an age field that is greater than or equal to zero
+public class Animal {
+    private String species;
+    private int age;
+
+    public Animal(String species, int age) {
+        setAge(age);
+        setSpecies(species);
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        if (species == null || species.isEmpty()){
+            throw new IllegalArgumentException("Species must be non-null and non empty");
+        }
+        this.species = species;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age < 0){
+            throw new IllegalArgumentException("Age must be greater than or equal to zero");
+        }
+        this.age = age;
+    }
+}
