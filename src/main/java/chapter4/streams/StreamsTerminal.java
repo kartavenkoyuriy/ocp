f53fec8b5cf890af62147297606c9f33faf8014c
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2018-11-20 23:31:57 +0200

    chapter 4. streams. finishing terminal operations

--- src/main/java/chapter4/streams/StreamsTerminal.java
+++ src/main/java/chapter4/streams/StreamsTerminal.java
@@ -1,9 +1,7 @@
 package chapter4.streams;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -16,16 +14,99 @@ public class StreamsTerminal {
     into a single primitive or Object. For example, you might have an int or a Collection.
      */
     public static void main(String[] args) {
-        //doesn't work with infinite streams
-        //reduction
 
-        Stream<String> monkeyStream = Stream.of("monkey", "gorilla", "bonobo", "ape", "2");
+//        Stream<String> monkeyStream = Stream.of("monkey", "gorilla", "bonobo", "ape", "2");
 
+
+        //MUTABLE reduction - more efficient because we use same object while accumulating
+
+        //describing of
+        //<R> R collect(Supplier<R> supplier, BiConsumer<R, ? super T> accumulator, BiConsumer<R, R> combiner);
+        Stream<String> wolfStream = Stream.of("w", "o", "l", "f");
+//        wolfStream.collect()
 
     }
 
     //doesn't work with infinite streams
-    //not a reduction
+    //reduction
+    //three overloaded methods - each described inside
+    private static void reduceReduceReduceExample() {
+        //---------------------------------------------------------------------------------------------------
+
+        //describing of
+        //T reduce(T identity, BinaryOperator<T> accumulator);
+        //where first operator - initial value,
+        //second(BinaryOperator) - holds updatedInitialValue, nextStreamElement, and operation with them
+        String[] wolfArray = new String[]{"w", "o", "l", "f"};
+        String resultArray = "";
+        for (String s : wolfArray) {
+            resultArray += s;
+        }
+        System.out.println(resultArray);
+
+        //vs
+
+        Stream<String> wolfStream = Stream.of("w", "o", "l", "f");
+        String resultStream = wolfStream.reduce("", (accum, nextElem) -> accum + nextElem);
+        //the same
+        //String resultStream = wolfStream.reduce("", String::concat);
+        System.out.println(resultStream);
+
+        //another example
+        Stream<Integer> integerStream = Stream.iterate(1, number -> number + 1);
+        Integer multiplyResult = integerStream.limit(5).reduce(1, (accum, next) -> accum * next);
+        //returns a value, but a stream is empty
+        //Integer multiplyResult = (Integer) Stream.empty().reduce(1, (accum, next) -> null);
+        System.out.println(multiplyResult);
+
+        //---------------------------------------------------------------------------------------------------
+
+        //describing of
+        //Optional<T> reduce(BinaryOperator<T> accumulator);
+        //returns Optional because Stream could be empty
+        //(this is method overload when need to show Optional[empty], when it is empty)
+        //
+        //when no need in initial value
+        //However, sometimes it is nice to differentiate the case where the
+        //stream is empty rather than the case where there is a value that happens to match the identity
+        //being returned from calculation. The signature returning an Optional lets us differentiate these cases.
+        //For example, we might return Optional.empty() when the stream is empty
+        //and Optional.of(3) when there is a value.
+        //
+        //another words - for differentiate empty streams and initial values
+        //(Integer) Stream.empty().reduce(1, (accum, next) -> null); - Optional[1]
+        //and
+        //Stream.empty().reduce(multiply).ifPresent(System.out::println); - Optional[empty]
+        BinaryOperator<Integer> multiply = (a, b) -> a * b;
+
+        Stream<Integer> empty = Stream.empty();
+        Stream<Integer> integerStreamOneValue = Stream.of(3);
+        Stream<Integer> integerStreamManyValues = Stream.of(3, 5, 6);
+
+        empty.reduce(multiply).ifPresent(System.out::println);
+        integerStreamOneValue.reduce(multiply).ifPresent(System.out::println);
+        integerStreamManyValues.reduce(multiply).ifPresent(System.out::println);
+
+        //what if BinaryOperator returns null - not interesting
+//        BinaryOperator<Integer> multiplyNull = (a, b) -> null;
+//        empty.reduce(multiplyNull).ifPresent(System.out::println);
+//        integerStreamOneValue.reduce(multiplyNull).ifPresent(System.out::println);
+//        integerStreamManyValues.reduce(multiplyNull).ifPresent(System.out::println);
+
+        //---------------------------------------------------------------------------------------------------
+
+        //describing of
+        //<U> U reduce(U identity, BiFunction<U, ? super T, U> accumulator, BinaryOperator<U> combiner);
+        //used when we are processing collections in parallel. It
+        //allows Java to create intermediate reductions and then combine them at the end
+
+        Stream<Integer> integerStreamForParallel = Stream.of(1, 2, 3, 4, 5);
+        Integer parallelReduceResult = integerStreamForParallel.reduce(1, multiply, multiply);
+        System.out.println("parallelReduceResult:" + parallelReduceResult);
+    }
+
+    //doesn't work with infinite streams
+    //not a reduction(??? because NO RETURN TYPE ...!!!)
     //takes a Consumer as a parameter
     //return type is void
     private static void forEachExample() {
@@ -70,19 +151,19 @@ public class StreamsTerminal {
 //        System.out.println(monkeyStream.noneMatch(stringPredicate));
 
         Stream<String> infiniteStream = Stream.generate(() -> "chimp");
-//        System.out.println(infiniteStream.anyMatch(stringPredicate));
+        System.out.println(infiniteStream.anyMatch(stringPredicate));
         //hang
-        System.out.println(infiniteStream.allMatch(stringPredicate));
-        System.out.println(infiniteStream.noneMatch(stringPredicate));
+//        System.out.println(infiniteStream.allMatch(stringPredicate));
+//        System.out.println(infiniteStream.noneMatch(stringPredicate));
     }
 
+    //not a reduction
     //work with infinite streams
-    //mot a reduction
     //return an Optional
     private static void findFirstFindAnyExample() {
         //findAny() is useful
         //when you are working with a parallel stream. It gives Java the ﬂexibility to return to you
-        //the frst element it comes by rather than the one that needs to be frst in the stream based
+        //the first element it comes by rather than the one that needs to be first in the stream based
         //on the intermediate operations
 
         Stream<String> monkeyStream = Stream.of("monkey", "gorilla", "bonobo", "ape");
@@ -102,8 +183,6 @@ public class StreamsTerminal {
     //takes a Comparator as a parameter
     //return an Optional
     private static void minMaxExample() {
-        //hang on finite streams
-        //both are reduction because they need to look into each value
         Stream<String> monkeyStream = Stream.of("monkey", "gorilla", "bonobo", "ape");
         System.out.println(monkeyStream.max((a, b) -> a.length() - b.length()).orElseThrow(IllegalArgumentException::new));
         // simplify comparing
@@ -113,6 +192,10 @@ public class StreamsTerminal {
 //        Stream<String> empty = Stream.empty();
 //        System.out.println(empty.max((a, b) -> b.length() - a.length()).orElseThrow(IllegalArgumentException::new));
 
+        Stream<Integer> integerStream = Stream.iterate(1, i -> i + 1);
+        Optional<Integer> max = integerStream.limit(3).max(Comparator.comparingInt(Integer::intValue));
+        max.ifPresent(System.out::println);
+
         //here MIN or MAX functions returns the element which is same as others
         Stream<Integer> generate = Stream.generate(() -> 1);
         Optional<Integer> min = generate.limit(5).max((a, b) -> 0);
@@ -121,7 +204,8 @@ public class StreamsTerminal {
 
     //it's a reduction terminal operation because it looks on each element and return a single value
     //doesn't work with infinite streams
-    //return an int
+    //return a long
+    //really is "return mapToLong(e -> 1L).sum()"
     private static void countExample() {
         Stream<String> monkeyStream = Stream.of("monkey", "gorilla", "bonobo");
         System.out.println(monkeyStream.count());
