3e971e4d55cf72d79af59cfef765449b8c2dc6d6
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2018-11-23 00:09:42 +0200

    chapter 4. continued with primitive streams.

---
+++ src/main/java/chapter4/primitiveStreams/CommonPrimitiveStreams.java
@@ -0,0 +1,10 @@
+package chapter4.primitiveStreams;
+
+
+import java.util.stream.Stream;
+
+public class CommonPrimitiveStreams {
+    public static void main(String[] args) {
+        Stream.of(3, 5, 6);
+    }
+}
