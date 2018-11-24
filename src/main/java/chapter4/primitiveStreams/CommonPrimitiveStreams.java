de762954e3e375381470cc5b4495eee78839af8a
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2018-11-24 12:09:38 +0200

    chapter 4. creating primitive streams.

--- src/main/java/chapter4/primitiveStreams/CommonPrimitiveStreams.java
+++ src/main/java/chapter4/primitiveStreams/CommonPrimitiveStreams.java
@@ -1,10 +1,24 @@
 package chapter4.primitiveStreams;
 
 
+import java.util.OptionalDouble;
 import java.util.stream.Stream;
 
 public class CommonPrimitiveStreams {
     public static void main(String[] args) {
-        Stream.of(3, 5, 6);
+        Stream<Integer> integerStream = Stream.of(3, 5, 6);
+        Integer reduce = integerStream.reduce(0, (init, x) -> init + x);
+        System.out.println(reduce);
+
+        Stream<Integer> integerStream1 = Stream.of(3, 5, 6);
+        int sum = integerStream1
+                .mapToInt(x -> x)
+                .sum();
+        System.out.println(sum);
+
+        Stream<Integer> integerStream2 = Stream.of(3, 5, 6);
+        OptionalDouble average = integerStream2.mapToInt(x -> x).average();
+        average.ifPresent(System.out::println);
+
     }
 }
--- src/main/java/chapter4/primitiveStreams/CreatePrimitiveStream.java
+++ src/main/java/chapter4/primitiveStreams/CreatePrimitiveStream.java
@@ -1,5 +1,43 @@
 package chapter4.primitiveStreams;
 
-public class CreatePrimitiveStream {
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 
+public class CreatePrimitiveStream {
+    public static void main(String[] args) {
+
+    }
+
+    private static void generateIterateExample() {
+        DoubleStream doubleStreamGenerate = DoubleStream.generate(Math::random);
+        IntStream intStreamGenerate = IntStream.generate(() -> (int) Math.round(Math.random() * 100));
+
+        doubleStreamGenerate.limit(10).forEach(System.out::println);
+        System.out.println("***");
+        intStreamGenerate.limit(10).forEach(System.out::println);
+        System.out.println("***");
+
+        DoubleStream doubleStreamIterate = DoubleStream.iterate(1024, x -> x / 2);
+        doubleStreamIterate.limit(16).forEach(System.out::println);
+//        doubleStreamIterate.limit(16).forEach(x -> System.out.println(x));
+
+        System.out.println("***");
+        Random r = new Random();
+        r.ints().limit(5).forEach(System.out::println);
+    }
+
+    private static void customCreatingPrimitiveStreamsExample() {
+        DoubleStream empty = DoubleStream.empty();
+        DoubleStream doubleStreamOneElem = DoubleStream.of(3.14);
+        DoubleStream doubleStreamManyElem = DoubleStream.of(1.1, 5.5, 2, 3.4);
+
+        empty.forEach(System.out::println);
+        System.out.println("***");
+        doubleStreamOneElem.forEach(System.out::println);
+        System.out.println("***");
+        doubleStreamManyElem.forEach(System.out::println);
+        System.out.println("***");
+    }
 }
