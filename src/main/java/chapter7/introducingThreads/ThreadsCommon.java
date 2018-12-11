faf0c6e6020f07f54fa1abc1503a7b55b87f1328
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2018-12-11 23:32:43 +0200

    chapter 7. continued threads

--- src/main/java/chapter7/introducingThreads/ThreadsCommon.java
+++ src/main/java/chapter7/introducingThreads/ThreadsCommon.java
@@ -1,16 +1,52 @@
 package chapter7.introducingThreads;
 
+class PrintData implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("printing:" + i);
+        }
+    }
+}
+
+class InventoryThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("zoo");
+    }
+}
+
 public class ThreadsCommon {
 
-    public static void main(String[] args) {
+    //a Thread instance will execute can be done two ways in Java:
+    //■ Provide a Runnable object or lambda expression to the Thread constructor.
+    //■ Create a class that extends Thread and overrides the run() method
 
+    public static void main(String[] args) {
+        notGuaranteedOrderExample();
+
+    }
+
+    private static void notGuaranteedOrderExample() {
+        System.out.println("begin");
+        new Thread(new PrintData()).start();
+        new InventoryThread().start();
+        System.out.println("end");
+    }
+
+    private static void commonThreadConstructorsExample() {
+        new Thread(new PrintData()).start();
+        new InventoryThread().start();
     }
 
     //example of instantiate Runnable interface through Thread constructor
     private static void runnableInterfaceExample() {
         new Thread(() -> System.out.println("1")).start();
 
-        new Thread(() -> {return;}).start();
-        new Thread(() -> {}).start();
+        new Thread(() -> {
+            return;
+        }).start();
+        new Thread(() -> {
+        }).start();
     }
 }
