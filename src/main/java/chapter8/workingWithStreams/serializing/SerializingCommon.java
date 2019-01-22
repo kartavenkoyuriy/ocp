eda17431b1615223d74c7f21d441854ce1963447
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2019-01-22 23:14:05 +0200

    chapter 8. continued with serializing

---
+++ src/main/java/chapter8/workingWithStreams/serializing/SerializingCommon.java
@@ -0,0 +1,46 @@
+package chapter8.workingWithStreams.serializing;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SerializingCommon {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Animal("tiger", 1, 'r'));
+        animals.add(new Animal("elephant", 2, 'A'));
+        animals.add(null);
+
+        System.out.println(animals);
+
+        File destination = new File( "D:\\install\\animals.data");
+        writeAnimalsToFile(animals, destination);
+        System.out.println(getAnimals(destination));
+    }
+
+    private static List<Animal> getAnimals(File source) throws IOException, ClassNotFoundException {
+        List<Animal> animals = new ArrayList<>();
+        try (ObjectInputStream is = new ObjectInputStream(new BufferedInputStream(new FileInputStream(source)))) {
+            while (true){
+                Object object = is.readObject();
+                //Serializing can work with null values
+                //but it should be handled
+                if(object instanceof Animal || object == null){
+                    animals.add((Animal) object);
+                }
+            }
+        } catch (EOFException e){
+            //end of file reached
+        }
+
+        return animals;
+    }
+
+    private static void writeAnimalsToFile(List<Animal> animals, File destination) throws IOException {
+        try(ObjectOutputStream os = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(destination)))){
+            for (Animal animal : animals) {
+                os.writeObject(animal);
+            }
+        }
+    }
+}
